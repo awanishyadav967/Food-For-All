5f5219eedd7250ba0706b33b321156e20cf05bd2
Avanish Yadav <91160609+awanishyadav967@users.noreply.github.com>
2026-02-06 19:45:46 +0530

    Refactor RegisterConfirmationActivity code

--- app/src/main/java/br/com/fomezero/joaofood/activities/merchant/RegisterConfirmationActivity.java
+++ app/src/main/java/br/com/fomezero/joaofood/activities/merchant/RegisterConfirmationActivity.java
@@ -1,27 +1,42 @@
-package br.com.fomezero.joaofood.activities.merchant
+package br.com.fomezero.joaofood.activities.merchant;
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import br.com.fomezero.joaofood.R
-import kotlinx.android.synthetic.main.activity_registrer_confirmation.homeButton
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-class RegisterConfirmationActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registrer_confirmation)
+import androidx.appcompat.app.AppCompatActivity;
 
-        homeButton.setOnClickListener {
-            val intentHome = Intent(this, MerchantHomeActivity::class.java)
-            startActivity(intentHome)
-            finish()
-        }
+import br.com.fomezero.joaofood.R;
+
+public class RegisterConfirmationActivity extends AppCompatActivity {
+
+    private Button homeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrer_confirmation);
+
+        homeButton = findViewById(R.id.homeButton);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToHome();
+            }
+        });
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-        val intentHome = Intent(this, MerchantHomeActivity::class.java)
-        startActivity(intentHome)
-        finish()
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        goToHome();
+    }
+
+    private void goToHome() {
+        Intent intentHome = new Intent(this, MerchantHomeActivity.class);
+        startActivity(intentHome);
+        finish();
     }
 }
