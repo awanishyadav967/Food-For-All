60ec2b50e30804b75a55f6009f3d0049c22f7bd3
Avanish Yadav <91160609+awanishyadav967@users.noreply.github.com>
2026-02-06 19:54:03 +0530

    Bug fixed

--- app/src/main/java/br/com/fomezero/joaofood/activities/merchant/MerchantHomeFragment.java
+++ app/src/main/java/br/com/fomezero/joaofood/activities/merchant/MerchantHomeFragment.java
@@ -1,139 +1,150 @@
-package br.com.fomezero.joaofood.activities.merchant
+package br.com.fomezero.joaofood.activities.merchant;
 
-import android.content.Intent
-import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.DefaultItemAnimator
-import androidx.recyclerview.widget.LinearLayoutManager
-import br.com.fomezero.joaofood.R
-import br.com.fomezero.joaofood.activities.ActiveUserData
-import br.com.fomezero.joaofood.model.MerchantData
-import br.com.fomezero.joaofood.model.OngData
-import br.com.fomezero.joaofood.model.Product
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.fragment_merchant_home.addProductButton
-import kotlinx.android.synthetic.main.fragment_merchant_home.productRecyclerView
-import kotlinx.android.synthetic.main.fragment_merchant_home.recyclerView
-import java.util.*
-import kotlin.collections.ArrayList
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import br.com.fomezero.joaofood.R;
+import br.com.fomezero.joaofood.activities.ActiveUserData;
+import br.com.fomezero.joaofood.model.MerchantData;
+import br.com.fomezero.joaofood.model.OngData;
+import br.com.fomezero.joaofood.model.Product;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.ktx.Auth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ktx.Firestore;
+import com.google.firebase.ktx.Firebase;
+import java.util.ArrayList;
+import java.util.List;
 
+public class MerchantHomeFragment extends Fragment {
+    private FirebaseFirestore db;
+    private FirebaseAuth auth;
+    private RecyclerView recyclerView;
+    private RecyclerView productRecyclerView;
+    private android.widget.Button addProductButton;
 
-class MerchantHomeFragment : Fragment() {
-    private val db: FirebaseFirestore by lazy {
-        Firebase.firestore
-    }
+    private ArrayList<OngData> ongList = new ArrayList<>();
+    private ArrayList<Product> productList = new ArrayList<>();
+    private OngsRecycleViewAdapter ongAdapter;
+    private ProductAdapter productAdapter;
 
-    private val auth: FirebaseAuth by lazy {
-        Firebase.auth
-    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        
+        db = Firebase.getFirestore();
+        auth = Firebase.getAuth();
+        
+        recyclerView = getView().findViewById(R.id.recyclerView);
+        productRecyclerView = getView().findViewById(R.id.productRecyclerView);
+        addProductButton = getView().findViewById(R.id.addProductButton);
 
-    private var ongList = arrayListOf<OngData>()
-    private var productList = arrayListOf<Product>()
-    private lateinit var ongAdapter: OngsRecycleViewAdapter
-    private lateinit var productAdapter: ProductAdapter
+        ongAdapter = new OngsRecycleViewAdapter(ongList, getActivity(), fragment -> {
+            loadFragment(fragment);
+            return null;
+        });
 
-    override fun onStart() {
-        super.onStart()
-        ongAdapter = OngsRecycleViewAdapter(ongList, activity!!) {
-            loadFragment(it)
-        }
+        productAdapter = new ProductAdapter(productList, getActivity());
 
-        productAdapter = ProductAdapter(productList, activity!!)
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(ongAdapter);
 
+        productRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        productRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        productRecyclerView.setAdapter(productAdapter);
 
-        recyclerView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
-        recyclerView.itemAnimator = DefaultItemAnimator()
-        recyclerView.adapter = ongAdapter
+        addProductButton.setOnClickListener(v -> {
+            Intent newProductIntent = new Intent(getActivity(), NewProductActivity.class);
+            startActivity(newProductIntent);
+        });
 
-        productRecyclerView
+        getProductList();
 
-        productRecyclerView.layoutManager =
-            LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
-        productRecyclerView.itemAnimator = DefaultItemAnimator()
-        productRecyclerView.adapter = productAdapter
-
-        addProductButton.setOnClickListener {
-            val newProductIntent = Intent(activity, NewProductActivity::class.java)
-            startActivity(newProductIntent)
-        }
-        getProductList()
-
-        ActiveUserData.getOngList(object: ActiveUserData.OngListCallback {
-            override fun onSuccess(ongList: ArrayList<OngData>) {
-                this@MerchantHomeFragment.ongList.clear()
-                this@MerchantHomeFragment.ongList.addAll(ongList)
-                ongAdapter.notifyDataSetChanged()
+        ActiveUserData.getOngList(new ActiveUserData.OngListCallback() {
+            @Override
+            public void onSuccess(ArrayList<OngData> ongListData) {
+                MerchantHomeFragment.this.ongList.clear();
+                MerchantHomeFragment.this.ongList.addAll(ongListData);
+                ongAdapter.notifyDataSetChanged();
             }
 
-            override fun onError(throwable: Throwable) { }
-
-        })
+            @Override
+            public void onError(Throwable throwable) {
+            }
+        });
     }
 
-    private fun getProductList() {
-        val users = db.collection("users")
-        val query = users.whereEqualTo("email", auth.currentUser?.email)
-        query.get()
-            .addOnSuccessListener { result ->
-                if (result.isEmpty.not()) {
-                    val document = result.first()
-                    document.getDocumentReference("data")?.get()?.addOnSuccessListener { data ->
-                        db.collection("products")
-                            .whereEqualTo("user", document.reference)
-                            .get()
-                            .addOnSuccessListener { products ->
-                                if (products.isEmpty.not()) {
-                                    val urlList: List<String>? = data.get("image") as List<String>?
-                                    val merchantData = MerchantData(
-                                        data.getString("name") ?: "",
-                                        data.getString("phoneNumber") ?: "",
-                                        data.getString("email") ?: "",
-                                        data.getString("address")?:"",
-                                        urlList?.first(),
-                                    )
-                                    productList.clear()
-                                    for (product in products) {
-                                        val urlList: List<String>? = product.get("image") as List<String>?
-                                        Log.d("Ashwith", urlList.toString())
-                                        productList.add(
-                                            Product(
-                                                product.getString("name") ?: "",
-                                                product.getString("amount") ?: "",
-                                                product.getString("price") ?: "0",
-                                                urlList?.first(),
-                                                merchantData,
-                                                product.getString("postDate")?:"none",
-                                            )
-                                        )
-                                        productAdapter.notifyDataSetChanged()
-                                    }
-                                }
+    private void getProductList() {
+        db.collection("users")
+                .whereEqualTo("email", auth.getCurrentUser().getEmail())
+                .get()
+                .addOnSuccessListener(result -> {
+                    if (!result.isEmpty()) {
+                        result.getDocuments().get(0).getDocumentReference("data").get().addOnSuccessListener(data -> {
+                            db.collection("products")
+                                    .whereEqualTo("user", result.getDocuments().get(0).getReference())
+                                    .get()
+                                    .addOnSuccessListener(products -> {
+                                        if (!products.isEmpty()) {
+                                            List<String> urlList = (List<String>) data.get("image");
+                                            String imagePath = (urlList != null && !urlList.isEmpty()) ? urlList.get(0) : null;
 
-                            }
+                                            MerchantData merchantData = new MerchantData(
+                                                    data.getString("name") != null ? data.getString("name") : "",
+                                                    data.getString("phoneNumber") != null ? data.getString("phoneNumber") : "",
+                                                    data.getString("email") != null ? data.getString("email") : "",
+                                                    data.getString("address") != null ? data.getString("address") : "",
+                                                    imagePath
+                                            );
+
+                                            productList.clear();
+                                            for (int i = 0; i < products.getDocuments().size(); i++) {
+                                                var product = products.getDocuments().get(i);
+                                                List<String> productUrlList = (List<String>) product.get("image");
+                                                String productImagePath = (productUrlList != null && !productUrlList.isEmpty()) ? productUrlList.get(0) : null;
+
+                                                Log.d("Ashwith", productUrlList != null ? productUrlList.toString() : "null");
+
+                                                productList.add(
+                                                        new Product(
+                                                                product.getString("name") != null ? product.getString("name") : "",
+                                                                product.getString("amount") != null ? product.getString("amount") : "",
+                                                                product.getString("price") != null ? product.getString("price") : "0",
+                                                                productImagePath,
+                                                                merchantData,
+                                                                product.getString("postDate") != null ? product.getString("postDate") : "none"
+                                                        )
+                                                );
+                                                productAdapter.notifyDataSetChanged();
+                                            }
+                                        }
+                                    });
+                        });
                     }
-                }
-            }
+                });
     }
 
-    private fun loadFragment(fragment: Fragment) {
-        val transaction = activity?.supportFragmentManager?.beginTransaction()
-        transaction?.replace(R.id.container, fragment)
-        transaction?.addToBackStack(null)
-        transaction?.commit()
+    private void loadFragment(Fragment fragment) {
+        if (getActivity() != null) {
+            getActivity().getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_merchant_home, container, false)
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_merchant_home, container, false);
     }
 }
